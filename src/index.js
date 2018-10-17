76b89eb9bc48f90fc0f7bc526cc11d8cfb226b15
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-17 14:07:51 -0600

    supports new dir

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
-import { persistor, store } from './store';
+import { persistor, store } from './config/store';
 import { render } from 'react-dom';
 import App from './App';
 
