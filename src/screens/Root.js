d4ea183afeb9c39f936ac8af2b52453ead22b9e8
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-17 15:07:02 -0600

    App now mvvm screens

---
+++ src/screens/Root.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Inputs from '../components/Inputs';
+import Display from '../components/Display';
+
+const ScreenRoot = () => {
+  return (
+    <div>
+      <Inputs  />
+      <Display />
+    </div>
+  );
+}
+
+export default ScreenRoot;
