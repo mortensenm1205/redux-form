7171a285220a3c564c78c0015963983309526ae5
mortensenm1205@gmail.com <mortensenm1205@gmail.com>
2018-10-17 15:07:09 -0600

    supports mvvm

--- src/index.js
+++ src/index.js
@@ -3,12 +3,12 @@ import { Provider } from 'react-redux';
 import { PersistGate } from 'redux-persist/lib/integration/react';
 import { persistor, store } from './config/store';
 import { render } from 'react-dom';
-import App from './App';
+import Root from './screens/Root';
 
 render(
     <Provider store={store}>
         <PersistGate persistor={persistor}>
-            <App/>
+            <Root/>
         </PersistGate>
     </Provider>, 
     document.getElementById('root'));
